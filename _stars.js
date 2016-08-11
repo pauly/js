4943b42d19e74c32cfe3959161e05ca4f999efa9
Pauly <pauly@clarkeology.com>
2016-08-11 12:23:40 +0000

    :horse_racing: reduce js size with binding, 639 bytes

--- _stars.js
+++ _stars.js
@@ -9,22 +9,22 @@
     element.appendChild(child);
   }
 
-  function _addRating(table, id, score, url, caller) {
-    url = '/a?t=' +
-      table + '&m=ok&key=' + id +
-      '&s=' + score;
-    caller = gU.byId('rate-' + table + '-' + id);
+  function _addRating(table, id, score, url, wrapper) {
+    wrapper = this; // because we did _addRating.bind(wrapper)
+    url = '/a?t=' + table + '&m=ok&key=' + id + '&s=' + score;
     gU.get(url, function() {
-      _html(caller, table + ' ' + score);
+      // briefly display "event 10" or whatever
+      _html(wrapper, table + ' ' + score);
+      // then after nearly a second, display the stars again
       setTimeout(function() {
-        _html(caller, _stars(table, id, score));
+        _html(wrapper, _stars(wrapper, table, id, score));
       }, 900);
     });
     return false;
   }
 
   // return some star ratings
-  function _stars(table, id, score, stars, index) {
+  function _stars(wrapper, table, id, score, stars, index) {
     score = Math.floor(score);
     stars = _createElement('ol');
     stars[className] = 'stars';
@@ -33,33 +33,25 @@
         a = _createElement('a');
       a[className] = 'star' + index;
       if (score === index) a[className] += ' lit lit' + index;
-      a.score = index;
-      gU.on(a, 'click', function(e) {
-        e.preventDefault();
-        return _addRating(table, id, this.score);
-      });
+      gU.on(a, 'click', _addRating.bind(wrapper, table, id, index));
       _appendChild(li, a);
       _appendChild(stars, li);
     }
     return stars;
   }
 
-  function _initialise() {
+  gU.ok(function() {
     var index,
       h6 = gU.byTag('h6'),
       regex;
     for (index in h6) {
       regex = /rate:([a-x]+):([0-9]+)(:([0-9]+))*/i.exec(h6[index][className]);
-      if (regex) {
-        var table = regex[1],
-          id = regex[2],
-          score = regex[4];
-        h6[index].id = 'rate-' + table + '-' + id;
-        _html(h6[index], _stars(table, id, score));
-      }
+      // table = regex[1],
+      // id = regex[2],
+      // score = regex[4],
+      // wrapper = h6[index];
+      regex && _html(h6[index], _stars(h6[index], regex[1], regex[2], regex[4]));
     }
-  }
-
-  gU.ok(_initialise);
+  });
 
 })(window, document);
