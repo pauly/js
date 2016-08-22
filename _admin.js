1821290b2155ef5077be02ec90f1dbca1b14085c
Pauly <pauly@clarkeology.com>
2016-08-22 09:30:17 +0000

    :mouse: alias gU.html, down to 993 bytes
    aliased gU.html to _setInnerHTML which is clearer, but
    also minifes to one char instead of 6! It is reused enough to
    make the saving worthwhile.
    
    also moved some commented out code that I may reinstate later

--- _admin.js
+++ _admin.js
@@ -2,29 +2,15 @@
   'use strict';
   gU = window.gU;
   if (!gU) return;
-  /* $(':input:enabled.[class*=maxlen]').each(function() {
-    if (/length(\d+)/.test($(this).attr('class'))) {
-      var len = RegExp.$1;
-      var showCharCount = function() {
-        var l = $(this).val()[length];
-        var r = len - l;
-        var c = '#' + r.toString(16) + r.toString(16) + r.toString(16);
-        c = r < 1 || c[length] < 7 ? '#f00' : c;
-        if (l >= len) {
-          $(this).addClass('error').next('.kC').css({ color: c }).html(r + ' - max length is ' + len + '!');
-        }
-        else {
-          $(this).removeClass('error').next('.kC').css({ color: c }).html(r + ' chars remaining');
-        }
-        return false;
-      };
-      $(this).after('<abbr title="Chars remaining" class="kC">' + len + '</abbr>').bind('keyup change', showCharCount).each(showCharCount);
-    }
-  }); */
+
+  var inputs = gU.tag('input'),
+    _setInnerHTML = gU.html,
+    _createElement = document.createElement.bind(document),
+    input, table, i, mText, mCategory, tagDiv;
 
   function buildList(list, js, input, item, listItem, link, index) {
     input = this; // this way minifies better than binding
-    gU.html(list, '');
+    _setInnerHTML(list, '');
     for (index = 0; index < js[length]; index++) {
       item = js[index];
       listItem = _createElement('li');
@@ -32,17 +18,17 @@
       link.id = item.guid;
       link.title = item[description] || '';
       // if (item.category) link.className = item.category.join(space);
-      gU.html(link, item.title);
-      gU.aC(listItem, link); // add link to listItem
-      gU.aC(list, listItem); // add listItem to list
+      _setInnerHTML(link, item.title);
+      gU.aC(listItem, link); // append child, add link to listItem
+      gU.aC(list, listItem); // append child, add listItem to list
       gU.on(link, 'click', function() {
         input[value] = this.id;
-        gU.html(list, '');
+        _setInnerHTML(list, '');
         list = 0;
       });
     }
     input.focus();
-    gU.sT && gU.sT();
+    gU.sT && gU.sT(); // reinstantiate sweet titles if we have it
   }
 
   function autocomplete(table, list, v) {
@@ -52,9 +38,6 @@
     }
   }
 
-  var inputs = gU.tag('input'),
-    _createElement = document.createElement.bind(document),
-    input, table, i, mText, mCategory, tagDiv;
   for (i = inputs[length] - 1; i >= 0; --i) {
     input = inputs[i];
     if (table = input.getAttribute('rel')) { // not good practice but better minifying
@@ -65,7 +48,26 @@
       parent[insertBefore](input, list); // swap
       gU.on(input, 'keyup', autocomplete.bind(input, table, list));
     }
-  };
+    // @todo rewrite maxlength not using jquery?
+    /* if (/length(\d+)/.test($(this).attr('class'))) {
+        var len = RegExp.$1;
+        var showCharCount = function() {
+          var l = $(this).val()[length];
+          var r = len - l;
+          var c = '#' + r.toString(16) + r.toString(16) + r.toString(16);
+          c = r < 1 || c[length] < 7 ? '#f00' : c;
+          if (l >= len) {
+            $(this).addClass('error').next('.kC').css({ color: c }).html(r + ' - max length is ' + len + '!');
+          }
+          else {
+            $(this).removeClass('error').next('.kC').css({ color: c }).html(r + ' chars remaining');
+          }
+          return false;
+        };
+        $(this).after('<abbr title="Chars remaining" class="kC">' + len + '</abbr>')
+          .bind('keyup change', showCharCount).each(showCharCount);
+      } */
+  }
   if (mCategory = gU.byId('mCategory')) { // not good practice but better minifying
     tagDiv = _createElement('div');
     mCategory[parentNode][insertBefore](tagDiv, mCategory);
@@ -77,7 +79,7 @@
         for (var i in tags) {
           tags[i] = '<a href="#" rel="tag" class="' + tags[i] + '" href="/' + tags[i] + '">' + tags[i].replace(/\+/g, space) + '</' + 'a>';
         }
-        gU.html(tagDiv, tags.join(space));
+        _setInnerHTML(tagDiv, tags.join(space));
       }
     });
   }
