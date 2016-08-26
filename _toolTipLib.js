004d12cae5939ea760b8e161e66b1970375e8198
Pauly <pauly@clarkeology.com>
2016-08-26 09:20:58 +0000

    :horse_racing: speed up hugely with event delegation
    attach event to the document and check if it bubbles to an anchor
    
    can remove a lot of code this way too, this library is vanishing fast

--- _toolTipLib.js
+++ _toolTipLib.js
@@ -1,56 +1,40 @@
 /*  Sweet Titles (c) Creative Commons 2005
   http://creativecommons.org/licenses/by-sa/2.5/
   Author: Dustin Diaz | http://www.dustindiaz.com
-  Updated: Paul Clarke, to fix ie offset and kill old tips if called twice
-  Update: PC again, 29/08/07 to not conflict with thickbox
+  rewritten by pauly
+  requires http://www.clarkeology.com/js/gbbsUpdater.js
 */
-(function (window, document, gU, parseInt, documentElement, body) { 
+(function (window, document, setTimeout, clearTimeout, parseInt, documentElement, body, gU, tipTimeout, opacityTimeout) { 
+  gU = window.gU;
   if (!gU) return;
 
-  var tipElements = ['a'];  // @Array: Allowable elements that can have the toolTip
-
-  function checkNode() {
-    var trueObj = sweetTitles.obj;
-    function inArray(arr, str) {
-      for (var i = 0, l = arr.length; i < l; i++) {
-        if (arr[i] === str) return true;
-      }
-      return false;
-    }
-    if (inArray(tipElements, trueObj.nodeName.toLowerCase())) {
-      return trueObj;
-    }
-    return trueObj.parentNode;
-  }
-
-  function tipOut() {
-    if (window.tID) clearTimeout(tID);
-    if (sweetTitles.opacityID) clearTimeout(sweetTitles.opacityID);
-    sweetTitles.tip.style.visibility = 'hidden';
-  }
-
-  function tipOver(e) {
-    sweetTitles.obj = this;
-    tID = window.setTimeout(tipShow, 50);
-    updateXY(e);
+  function tippableElement(element) {
+    return element && element.nodeName == 'A';
+    // I'm only allowing anchors to have tooltips on, but if we wanted more:
+    //  var tippableElements = ['A'], index;
+    //  if (element) {
+    //    for (index = tippableElements.length; index > 0; index--) {
+    //      if (tippableElements[index - 1] === element.nodeName) return true;
+    //    }
+    //  }
   }
 
   function tipShow() {    
-    var scrX = Number(sweetTitles.xCord),
-      scrY = Number(sweetTitles.yCord),
-      tp = parseInt(scrY+10),
-      lt = parseInt(scrX+10),
-      anch = checkNode();
+    var scrX = parseInt(sweetTitles.xCord),
+      scrY = parseInt(sweetTitles.yCord),
+      top = parseInt(scrY+10),
+      left = parseInt(scrX+10),
+      anch = this;
     sweetTitles.tip.innerHTML = anch.getAttribute('tip');
 
-    if (parseInt(document[documentElement].clientWidth + document[documentElement].scrollLeft) < parseInt(sweetTitles.tip.offsetWidth + lt)) {
-      lt = parseInt(lt-(sweetTitles.tip.offsetWidth+25));
+    if (parseInt(document[documentElement].clientWidth + document[documentElement].scrollLeft) < parseInt(sweetTitles.tip.offsetWidth + left)) {
+      left = parseInt(left - (sweetTitles.tip.offsetWidth + 25));
     }
-    sweetTitles.tip.style.left = lt+'px';
-    if (parseInt(document[documentElement].clientHeight + document[documentElement].scrollTop) < parseInt(sweetTitles.tip.offsetHeight + tp)) {
-      tp = parseInt(tp-(sweetTitles.tip.offsetHeight+25));
+    sweetTitles.tip.style.left = left + 'px';
+    if (parseInt(document[documentElement].clientHeight + document[documentElement].scrollTop) < parseInt(sweetTitles.tip.offsetHeight + top)) {
+      top = parseInt(top - (sweetTitles.tip.offsetHeight + 25));
     }
-    sweetTitles.tip.style.top = tp + 'px';
+    sweetTitles.tip.style.top = top + 'px';
     sweetTitles.tip.style.visibility = 'visible';
     sweetTitles.tip.style.opacity = '.1';
     tipFade(10);
@@ -58,10 +42,10 @@
 
   function tipFade(opac) {
     var passed = parseInt(opac),
-    newOpac = parseInt(passed+10);
+      newOpac = parseInt(passed + 10);
     if (newOpac < 80) {
-      sweetTitles.tip.style.opacity = '.'+newOpac;
-      sweetTitles.opacityID = window.setTimeout(function () {
+      sweetTitles.tip.style.opacity = '.' + newOpac;
+      opacityTimeout = setTimeout(function () {
         tipFade(newOpac);
       }, 20);
       return;
@@ -69,51 +53,56 @@
     sweetTitles.tip.style.opacity = '.80';
   }
 
-  function updateXY(e) {
+  function updateXY(event) {
     if (document.captureEvents) {
-      sweetTitles.xCord = e.pageX;
-      sweetTitles.yCord = e.pageY;
+      sweetTitles.xCord = event.pageX;
+      sweetTitles.yCord = event.pageY;
       return;
     }
     if (window.event.clientX) {
-      sweetTitles.xCord = e.clientX + (document[documentElement].scrollLeft || document[body].scrollLeft);
-      sweetTitles.yCord = e.clientY + (document[documentElement].scrollTop || document[body].scrollTop);
+      sweetTitles.xCord = event.clientX + (document[documentElement].scrollLeft || document[body].scrollLeft);
+      sweetTitles.yCord = event.clientY + (document[documentElement].scrollTop || document[body].scrollTop);
     }
   }
 
   var sweetTitles = {
     xCord: 0,
     yCord: 0,
-    obj: Object,        // @Element: That of which you're hovering over
     tip: Object,        // @Element: The actual toolTip itself
     init: function() {
       if ((window.innerWidth || document[body].clientWidth) < 800) return;
-      // var oldTip = gU.id('toolTip'), i;
-      // if (oldTip) document[body].removeChild(oldTip);
-      sweetTitles.tip = document.createElement('div');
+      sweetTitles.tip = gU.cE('div'); // createElement
       sweetTitles.tip.id = 'toolTip';
-      gU.aC(gU.tag(body)[0], sweetTitles.tip);
-      sweetTitles.tip.style.top = '0';
+      gU.aC(gU.tag(body)[0], sweetTitles.tip); // appendChild
+      // sweetTitles.tip.style.top = '0';
       sweetTitles.tip.style.position = 'absolute';
       sweetTitles.tip.style.visibility = 'hidden';
-      tipOut();
-      for (i = 0; i < tipElements.length; i ++) {
-        var c = gU.tag(tipElements[i]),
-          curLen = c.length;
-        for (var j = 0; j < curLen; j ++) {
-          if (!c[j].title) continue;
-          gU.on(c[j], 'mouseover', tipOver);
-          gU.on(c[j], 'mouseout', tipOut);
-          gU.on(c[j], 'click', tipOut);
-          c[j].setAttribute('tip', c[j].title);
-          c[j].removeAttribute('title');
-          // c[j].className = c[j].className + ' sweetTitle'; 
-        }
-      }
       gU.on(document, 'mousemove', updateXY);
+      gU.on(document, 'mouseover', function (event, element, title) {
+        element = event.target;
+        // event delegation, only apply to the elements in tipElements, so <a>
+        if (tippableElement(element)) {
+          title = element.getAttribute('title');
+          if (title) {
+            element.setAttribute('tip', title);
+            element.removeAttribute('title');
+          }
+          if (element.getAttribute('tip')) {
+            tipTimeout = setTimeout(tipShow.bind(element), 50);
+            updateXY(event);
+          }
+        }
+      });
+      gU.on(document, 'mouseout', function (event) {
+        // event delegation, only apply to the elements in tipElements, so <a>
+        if (tippableElement(event.target)) {
+          clearTimeout(tipTimeout);
+          clearTimeout(opacityTimeout);
+          sweetTitles.tip.style.visibility = 'hidden';
+        }
+      });
     }
   };
-  gU.sT = sweetTitles.init;
-  window.setTimeout(gU.sT, 500);
-  return gU.sT; // only init is exported now so call sweetTitles()
-})(window, document, window.gU, parseInt, 'documentElement', 'body');
+  gU.sT = sweetTitles.init; // attach it to gbbsUpdater as it is called again, though it does not need to be any more
+  gU.ok(sweetTitles.init); // can launch asap because of event delegation
+})(window, document, setTimeout, clearTimeout, parseInt, 'documentElement', 'body');
