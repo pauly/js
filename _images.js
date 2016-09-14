3a598451bc077f5e61d4f3f1463261ea2fc41ad5
Pauly <pauly@clarkeology.com>
2016-09-14 15:17:14 +0000

    :mouse: better minifiability < 1.7k now

--- _images.js
+++ _images.js
@@ -1,7 +1,7 @@
 'use strict';
 // I really liked the overlays that flickr used to do,
 // so this is that.
-(function(document, gU, round, appendChild, coords, style) {
+(function(document, gU, round) {
   // requires http://www.clarkeology.com/js/gbbsUpdater.js
   // also requires an api at /i that can get or post
   // notes for images.
@@ -27,13 +27,24 @@
   if (!gU) return;
 
   var images,
-    box = null;
+    box = 0,
+    // now a load of strings for better minifying
+    appendChild = 'appendChild',
+    coords = 'coords',
+    currentTarget = 'currentTarget',
+    // src = 'src',
+    scrollTop = 'scrollTop',
+    scrollLeft = 'scrollLeft',
+    heightString = 'height',
+    widthString = 'width',
+    style = 'style',
+    body = 'body';
 
   function getOffset(element, offset) {
     offset = { x: 0, y: 0 };
     do {
-      offset.x += element.offsetLeft - element.scrollLeft;
-      offset.y += element.offsetTop - element.scrollTop;
+      offset.x += element.offsetLeft - element[scrollLeft];
+      offset.y += element.offsetTop - element[scrollTop];
     } while (element = element.offsetParent);
     return offset;
   }
@@ -51,14 +62,14 @@
     }
   }
 
-  function mousedown(e, image, src, offset, x, y, text, url) {
-    if (!e.altKey) return;
-    src = e.currentTarget.currentSrc;
+  function mousedown(event, image, src, offset, x, y, text, url) {
+    if (!event.altKey) return;
+    src = event[currentTarget].currentSrc;
     image = findImage(src);
-    offset = getOffset(e.currentTarget);
-    x = e.pageX - offset.x - document.body.scrollLeft;
-    y = e.pageY - offset.y - document.body.scrollTop;
-    if (box === null) {
+    offset = getOffset(event[currentTarget]);
+    x = event.pageX - offset.x - document[body][scrollLeft];
+    y = event.pageY - offset.y - document[body][scrollTop];
+    if (!box) { // this is our first click...
       box = {
         offset: offset,
         left: x,
@@ -66,24 +77,24 @@
       };
       return;
     }
+    // so this must be our second click...
     box.right = x;
     box.bottom = y;
     box.src = src;
     box[coords] = [
-      round((box.left / image.width) * 100), // left as %
-      round((box.top / image.height) * 100), // top as %
-      round((box.right / image.width) * 100), // right as %
-      round((box.bottom / image.height) * 100) // bottom as %
+      round((box.left / image[widthString]) * 100), // left as %
+      round((box.top / image[heightString]) * 100), // top as %
+      round((box.right / image[widthString]) * 100), // right as %
+      round((box.bottom / image[heightString]) * 100) // bottom as %
     ];
     drawNote(box);
-    box.text = prompt('Enter a note for this area');
-    if (box.text) {
+    if (box.text = prompt('Enter a note for this area')) {
       url = '/i/' + beginningOf(image.src) + '?_method=post' +
         '&text=' + box.text + '&' + coords + '[0]=' + box[coords][0] + '&' + coords + '[1]=' + box[coords][1] +
         '&' + coords + '[2]=' + box[coords][2] + '&' + coords + '[3]=' + box[coords][3];
       gU.get(url, drawNotes);
     }
-    box = null;
+    box = 0;
   }
 
   function drawNote(note, offset, image, overlay, src, left, top, width, height) {
@@ -91,21 +102,21 @@
     if (image = findImage(src)) {
       offset = getOffset(image);
       if (!offset) return;
-      left = offset.x + ((image.width * note[coords][0]) / 100) + document.body.scrollLeft;
-      top = offset.y + ((image.height * note[coords][1]) / 100) + document.body.scrollTop;
-      width = image.width * ((note[coords][2] - note[coords][0]) / 100);
-      height = image.height * ((note[coords][3] - note[coords][1]) / 100);
-      overlay = document.createElement('a');
+      left = offset.x + ((image[widthString] * note[coords][0]) / 100) + document[body][scrollLeft];
+      top = offset.y + ((image[heightString] * note[coords][1]) / 100) + document[body][scrollTop];
+      width = image[widthString] * ((note[coords][2] - note[coords][0]) / 100);
+      height = image[heightString] * ((note[coords][3] - note[coords][1]) / 100);
+      overlay = gU.cE('a'); // createElement, from gbbsUpdater.js
       overlay.className = 'overlay';
       overlay[style].color = '#' + (note.color || 'ff0');
       overlay[style].border = '1px solid ' + overlay[style].color;
       overlay[style].top = top + 'px';
       overlay[style].left = left + 'px';
-      overlay[style].width = width + 'px';
-      overlay[style].height = height + 'px';
+      overlay[style][widthString] = width + 'px';
+      overlay[style][heightString] = height + 'px';
       overlay.title = note.text;
-      document.body[appendChild](overlay);
-      return overlay;
+      document[body][appendChild](overlay);
+      // return overlay;
     }
   }
 
@@ -121,12 +132,14 @@
   }
 
   function init(index, image, lazy) {
-    images = gU.tag('img');
+    images = gU.tag('img'); // getElementsByTagName from gbbsUpdater.js
+    // gU.debug && gU.debug('@todo delegate image event here?');
     for (index = images.length - 1; index >= 0; --index) {
       image = images[index];
+      image[style][heightString] = '';
       lazy = image.getAttribute('data-lazy');
       if (lazy) image.src = lazy;
-      if (!image.src.match(/maps.google|icons|ebaystatic|amazon|lastfm|lst.fm/)) {
+      if (!image.src.match(/icons|ebayamazon|lastfm|lst.fm/)) {
         gU.on(image, 'mousedown', mousedown);
         setTimeout(function() {
           gU.get('/i/' + this, drawNotes);
@@ -136,4 +149,4 @@
   }
 
   gU.ok(init);
-})(document, window.gU, Math.round, 'appendChild', 'coords', 'style');
+})(document, window.gU, Math.round);
